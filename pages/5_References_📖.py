442a3fc034ab095ccdd63c9f644f3f5a090ada8d
crans03 <144947218+crans03@users.noreply.github.com>
2025-05-09 21:27:54 -0700

    Update 5_References_📖.py

--- pages/5_References_📖.py
+++ pages/5_References_📖.py
@@ -16,7 +16,7 @@ references_path = 'references_text.md'
 st.set_page_config(page_title="References", page_icon='📖')
 
 # title 
-st.title(read_file(references_path, 2, 2)
+st.title(read_file(references_path, 2, 2))
 
 #paragraph
 st.markdown(read_file(references_path, 6, 32))
