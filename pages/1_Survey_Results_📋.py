8aee97696e67e245a3e1a0fb56f229fe49336f29
crans03 <144947218+crans03@users.noreply.github.com>
2025-05-09 21:18:57 -0700

    Update 1_Survey_Results_📋.py

--- pages/1_Survey_Results_📋.py
+++ pages/1_Survey_Results_📋.py
@@ -1,5 +1,15 @@
 import streamlit as st
-from Home import read_file
+
+def read_file(file_path, start_num, end_num):
+    file = open(file_path, 'r')
+    skippedContent = ""
+    content = ""
+    if start_num > 0:
+        for x in range(start_num-1):
+            skippedContent += file.readline()
+    for x in range(start_num, end_num+1):
+        content += file.readline()
+    return content
 
 survey_path = 'survey_text.md'
 
